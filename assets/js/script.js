e690219de8ac158c3ada4bb7d63cd02ed33a6f6e
Lucas da Silva Araújo <araujocreeds@gmail.com>
2024-11-20 20:02:30 -0300

    Finished with a working button to change the theme from light to dark.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
 (function () {
+  const $body = document.querySelector("body");
   const $txtDisplay = document.querySelector(".txtDisplay");
   const $btns = document.querySelectorAll("button");
   const $letters = document.querySelectorAll(".letter");
+  const $thema = document.querySelector(".thema");
 
   let capslock = false;
   let count = 0;
-
+  let countThema = 0;
   // const $cursor = document.createElement("span");
   // $cursor.className = "cursor";
+  $thema.addEventListener("click", () => {
+    const $thema_icon = $thema.querySelector(".thema-icon");
+    if (countThema % 2 === 0) {
+      $thema_icon.classList.add("thema-icon-dark");
+      $thema_icon.classList.remove("thema-icon-light");
+      $body.classList.toggle('dark-mode')
+    } else {
+      $thema_icon.classList.add("thema-icon-light");
+      $thema_icon.classList.remove("thema-icon-dark");
+      $body.classList.toggle('dark-mode')
+    }
+    countThema++;
+    console.log(countThema);
+  });
 
   $btns.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -60,8 +76,8 @@
     } else if (e.key === "Enter") {
       $txtDisplay.innerHTML += "<br/>";
     } else if (e.key.length === 1) {
-      let char = e.key
-      if(capslock){
+      let char = e.key;
+      if (capslock) {
         char = char.toUpperCase();
       }
       $txtDisplay.innerHTML += char;
